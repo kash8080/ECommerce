d0f167d40df20b117c408d9f2d5162fbfb5d930c
Rahul <rahulkash931@gmail.com>
2018-06-17 03:14:36 +0530

    remomved winston file transports

--- config/libs/logger.js
+++ config/libs/logger.js
@@ -33,8 +33,8 @@ const logger = winston.createLogger({
     // - Write to all logs with level `info` and below to `combined.log`
     // - Write all logs error (and below) to `error.log`.
     //
-    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'logs/combined.log' })
+    //new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    //new winston.transports.File({ filename: 'logs/combined.log' })
   ]
 });
 
@@ -43,9 +43,10 @@ const logger = winston.createLogger({
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
 //
 if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple()
-  }));
+  //
 }
+logger.add(new winston.transports.Console({
+  format: winston.format.simple()
+}));
 
 module.exports= logger;
